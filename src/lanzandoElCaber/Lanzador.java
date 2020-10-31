6c61aed5c855ad76399a27e6a504086ecf1c38ca
Mel-A <meabuin@alumno.unlam.edu.ar>
2020-10-31 14:15:06 -0300

    agrego funcion para agregar competidores a torneo

--- src/lanzandoElCaber/Lanzador.java
+++ src/lanzandoElCaber/Lanzador.java
@@ -1,5 +1,12 @@
 package lanzandoElCaber;
+import java.util.List;
 
 public class Lanzador {
-
+	private int numeroLanzador;
+	private List<Lanzamiento> lanzamientos;
+	
+	public Lanzador(int numeroLanzador, List<Lanzamiento> lanzamientos){
+		this.numeroLanzador = numeroLanzador;
+		this.lanzamientos = lanzamientos;
+	}
 }
--- src/lanzandoElCaber/Torneo.java
+++ src/lanzandoElCaber/Torneo.java
@@ -1,13 +1,33 @@
 package lanzandoElCaber;
 import java.util.Map;
+import java.util.List;
+import java.util.Map.Entry;
 
 public class Torneo {
+	List<Lanzador> competidores;  
+	
+	public Torneo(){}
+	
     public static void main(String[] args) {
         Map<Integer, Lanzamiento> lanzamientos = FileManager.singleton.leerArchivo("entrada.txt");
-
-        for (Integer value : lanzamientos.keySet()) {
-        	System.out.println(value);
-        }
+        
         
     }
+    
+    void agregarLanzadoresACompetencia(Map<Integer, Lanzamiento> lanzamientos){
+    	List<Lanzamiento> lanzamientosPorLanzador = null;
+        int lanzadorActual = 1;
+        for (Entry<Integer, Lanzamiento> lanzamiento : lanzamientos.entrySet()) {
+        	int keyLanzador = lanzamiento.getKey();
+        	if(lanzadorActual == keyLanzador){
+            	lanzamientosPorLanzador.add(lanzamiento.getValue());
+        	} else {
+        		Lanzador competidor = new Lanzador(lanzadorActual, lanzamientosPorLanzador);
+        		this.competidores.add(competidor);
+        		lanzadorActual = keyLanzador;
+        	}
+        }
+    }
+    
+    
 }
