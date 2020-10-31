753b4af9a7cb04d23f5b97d20856263ec1a4d270
Mel-A <meabuin@alumno.unlam.edu.ar>
2020-10-31 15:16:08 -0300

    se agregan test para agregarcompetidores

--- src/lanzandoElCaber/Lanzador.java
+++ src/lanzandoElCaber/Lanzador.java
@@ -1,12 +1,16 @@
 package lanzandoElCaber;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lanzador {
 	private int numeroLanzador;
-	private List<Lanzamiento> lanzamientos;
+	private List<Lanzamiento> lanzamientos = new ArrayList<>();
 	
-	public Lanzador(int numeroLanzador, List<Lanzamiento> lanzamientos){
+	public Lanzador(int numeroLanzador){
 		this.numeroLanzador = numeroLanzador;
-		this.lanzamientos = lanzamientos;
+	}
+	
+	public void registrarLanzamiento(Lanzamiento lanzamiento){
+		this.lanzamientos.add(lanzamiento);
 	}
 }
--- src/lanzandoElCaber/Torneo.java
+++ src/lanzandoElCaber/Torneo.java
@@ -1,33 +1,41 @@
 package lanzandoElCaber;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.List;
 import java.util.Map.Entry;
 
 public class Torneo {
-	List<Lanzador> competidores;  
+	private List<Lanzador> competidores = new ArrayList<>();  
 	
 	public Torneo(){}
 	
     public static void main(String[] args) {
         Map<Integer, Lanzamiento> lanzamientos = FileManager.singleton.leerArchivo("entrada.txt");
-        
-        
     }
     
     void agregarLanzadoresACompetencia(Map<Integer, Lanzamiento> lanzamientos){
-    	List<Lanzamiento> lanzamientosPorLanzador = null;
         int lanzadorActual = 1;
+    	Lanzador competidor = new Lanzador(lanzadorActual);
+		this.competidores.add(competidor);
+
         for (Entry<Integer, Lanzamiento> lanzamiento : lanzamientos.entrySet()) {
         	int keyLanzador = lanzamiento.getKey();
-        	if(lanzadorActual == keyLanzador){
-            	lanzamientosPorLanzador.add(lanzamiento.getValue());
-        	} else {
-        		Lanzador competidor = new Lanzador(lanzadorActual, lanzamientosPorLanzador);
-        		this.competidores.add(competidor);
+        	
+        	if(lanzadorActual != keyLanzador){
         		lanzadorActual = keyLanzador;
+
+        		competidor = new Lanzador(lanzadorActual);
+        		competidor.registrarLanzamiento(lanzamiento.getValue());
+        		this.competidores.add(competidor);
+        		
+        	}else{
+        		competidor.registrarLanzamiento(lanzamiento.getValue());
         	}
         }
     }
     
+    public List<Lanzador> getCompetidores(){
+    	return this.competidores;
+    }
     
 }
