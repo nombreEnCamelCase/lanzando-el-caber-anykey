11d4f8adab49c237b771f06138fb18fa6e40cf3d
Kampeador2047 <nahuel.ambroso@gmail.com>
2020-11-06 22:42:55 -0300

    archivos modificados de la primera entrega

--- src/lanzandoElCaber/App.java
+++ src/lanzandoElCaber/App.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class App {
 	public static void main(String[] args) {
-		Torneo torneo = new Torneo("casos-prueba\\\\caso_6.in");
+		Torneo torneo = new Torneo("casos-prueba\\\\caso_5.in");
 		torneo.agregarLanzadoresACompetencia();
 		torneo.generarPodios();
 	}
